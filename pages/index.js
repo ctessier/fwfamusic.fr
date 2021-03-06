15e567744fa11762c020a694cf23c22b8c2bf5b5
Clément Tessier <clement.tessier@ymail.com>
2021-03-06 18:34:39 +0100

    fix video aspect ratio

--- pages/index.js
+++ pages/index.js
@@ -8,17 +8,21 @@ export default function Home() {
         <title>Better Days – FWFA</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex h-screen justify-center items-center">
-        <div className="relative w-1/2" style={{ paddingTop: '28.15%' }}>
-          <ReactPlayer
-            url="https://www.youtube.com/watch?v=4vwFyex0tCA"
-            light="https://i.ytimg.com/vi/4vwFyex0tCA/maxresdefault.jpg"
-            width="100%"
-            height="100%"
-            className="absolute top-0 left-0 shadow-2xl"
-            playing
-            controls
-          />
+      <div className="flex h-screen justify-center items-center p-4">
+        <div className="w-full xl:w-1/2 md:w-2/3">
+          <div className="relative" style={{ paddingBottom: '56.3%' }}>
+            <div className="absolute object-cover shadow-2xl w-full h-full">
+              <ReactPlayer
+                url="https://www.youtube.com/watch?v=4vwFyex0tCA"
+                light="https://i.ytimg.com/vi/4vwFyex0tCA/maxresdefault.jpg"
+                width="100%"
+                height="100%"
+                className="absolute top-0 left-0 shadow-2xl"
+                playing
+                controls
+              />
+            </div>
+          </div>
         </div>
       </div>
     </>
